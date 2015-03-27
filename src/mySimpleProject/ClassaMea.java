8d7c932aa4ba6e564745ae2a24aaa4a9ff79c7e5
Diaconu Stefan <diaconustefandaniel@gmail.com>
2015-03-27 16:33:48 +0200

    Added Persoana.java and some changes to ClassaMea

--- src/mySimpleProject/ClassaMea.java
+++ src/mySimpleProject/ClassaMea.java
@@ -6,6 +6,9 @@ public class ClassaMea {
 		System.out.println("Hai salut");
 		System.out.println("Bre..tu chiar stii!!!");
 		System.out.println("coca-cola");
+		int x=5;
+		int y=7;
+		System.out.println(x+y);
 	}
 
 }
---
+++ src/mySimpleProject/Persoana.java
@@ -0,0 +1,30 @@
+package mySimpleProject;
+
+public class Persoana {
+
+	private String nume;
+	private String prenume;
+	private int age;
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public String getPrenume() {
+		return prenume;
+	}
+	public void setPrenume(String prenume) {
+		this.prenume = prenume;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+}
