6a1e2221fb537d4ad40b80d8165a1713b1ec6feb
Diaconu Stefan <diaconustefandaniel@gmail.com>
2015-03-27 14:44:51 +0200

    am adaugat "coca-cola"

--- src/mySimpleProject/ClassaMea.java
+++ src/mySimpleProject/ClassaMea.java
@@ -5,6 +5,7 @@ public class ClassaMea {
 	public static void main(String[] args) {
 		System.out.println("Hai salut");
 		System.out.println("Bre..tu chiar stii!!!");
+		System.out.println("coca-cola");
 	}
 
 }
